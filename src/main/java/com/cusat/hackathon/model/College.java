4472f919f88381f9775f1007cc19158e512b4fcc
keshavpc <keshavpc@192.168.0.13>
2017-03-18 02:16:22 +0530

    college commited

---
+++ src/main/java/com/cusat/hackathon/model/College.java
@@ -0,0 +1,31 @@
+package com.cusat.hackathon.model;
+
+public class College {
+	private String sectorCode;
+	private String collegeName;
+	/**
+	 * @return the sectorCode
+	 */
+	public String getSectorCode() {
+		return sectorCode;
+	}
+	/**
+	 * @param sectorCode the sectorCode to set
+	 */
+	public void setSectorCode(String sectorCode) {
+		this.sectorCode = sectorCode;
+	}
+	/**
+	 * @return the collegeName
+	 */
+	public String getCollegeName() {
+		return collegeName;
+	}
+	/**
+	 * @param collegeName the collegeName to set
+	 */
+	public void setCollegeName(String collegeName) {
+		this.collegeName = collegeName;
+	}
+
+}
