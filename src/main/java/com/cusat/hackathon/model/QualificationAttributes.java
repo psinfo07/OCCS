98807bb4ed0534f618690cfaab8fc294b8fa2fe8
PRAKASH <PRAKASH@192.168.0.9>
2017-03-18 02:29:23 +0530

    two file added

---
+++ src/main/java/com/cusat/hackathon/model/QualificationAttributes.java
@@ -0,0 +1,31 @@
+package com.cusat.hackathon.model;
+
+public class QualificationAttributes {
+	private String qualificationAttributeId;
+	private String attributeName;
+	/**
+	 * @return the qualificationAttributeId
+	 */
+	public String getQualificationAttributeId() {
+		return qualificationAttributeId;
+	}
+	/**
+	 * @param qualificationAttributeId the qualificationAttributeId to set
+	 */
+	public void setQualificationAttributeId(String qualificationAttributeId) {
+		this.qualificationAttributeId = qualificationAttributeId;
+	}
+	/**
+	 * @return the attributeName
+	 */
+	public String getAttributeName() {
+		return attributeName;
+	}
+	/**
+	 * @param attributeName the attributeName to set
+	 */
+	public void setAttributeName(String attributeName) {
+		this.attributeName = attributeName;
+	}
+	
+}
---
+++ src/main/java/com/cusat/hackathon/model/Sector.java
@@ -0,0 +1,31 @@
+package com.cusat.hackathon.model;
+
+public class Sector {
+	private String sectorCode;
+	private String sectorName;
+	/**
+	 * @return the sectorCode
+	 */
+	public String getSectorCode() {
+		return sectorCode;
+	}
+	/**
+	 * @param sectorCode the sectorCode to set
+	 */
+	public void setSectorCode(String sectorCode) {
+		this.sectorCode = sectorCode;
+	}
+	/**
+	 * @return the sectorName
+	 */
+	public String getSectorName() {
+		return sectorName;
+	}
+	/**
+	 * @param sectorName the sectorName to set
+	 */
+	public void setSectorName(String sectorName) {
+		this.sectorName = sectorName;
+	}
+	
+}
