675621534c7239e55d27ef90dbfcea3502300bbc
keshavpc <keshavpc@192.168.0.13>
2017-03-18 01:33:15 +0530

    Personal details commited

--- src/main/java/com/cusat/hackathon/model/PersonalDetail.java
+++ src/main/java/com/cusat/hackathon/model/PersonalDetail.java
@@ -1,5 +1,96 @@
 package com.cusat.hackathon.model;
 
+import com.sun.jmx.snmp.Timestamp;
+
 public class PersonalDetail {
+	private String name;
+	private String fatherName;
+	private String motherName;
+	private String emailId;
+	private String mobileNo;
+	private String password;
+	private Timestamp lastUpdateTs;
+	
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the fatherName
+	 */
+	public String getFatherName() {
+		return fatherName;
+	}
+	/**
+	 * @param fatherName the fatherName to set
+	 */
+	public void setFatherName(String fatherName) {
+		this.fatherName = fatherName;
+	}
+	/**
+	 * @return the motherName
+	 */
+	public String getMotherName() {
+		return motherName;
+	}
+	/**
+	 * @param motherName the motherName to set
+	 */
+	public void setMotherName(String motherName) {
+		this.motherName = motherName;
+	}
+	/**
+	 * @return the emailId
+	 */
+	public String getEmailId() {
+		return emailId;
+	}
+	/**
+	 * @param emailId the emailId to set
+	 */
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	/**
+	 * @return the mobileNo
+	 */
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	/**
+	 * @param mobileNo the mobileNo to set
+	 */
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	/**
+	 * @return the lastUpdateTs
+	 */
+	public Timestamp getLastUpdateTs() {
+		return lastUpdateTs;
+	}
+	/**
+	 * @param lastUpdateTs the lastUpdateTs to set
+	 */
+	public void setLastUpdateTs(Timestamp lastUpdateTs) {
+		this.lastUpdateTs = lastUpdateTs;
+	}
 
 }
