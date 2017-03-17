1db1788bc7be417a9ecdafcd7aa7af483d1baf0a
BHASKAR <BHASKAR@192.168.0.10>
2017-03-18 02:44:18 +0530

    db connectivity

--- src/main/java/com/cusat/hackathon/dbconn/DatabaseConnection.java
+++ src/main/java/com/cusat/hackathon/dbconn/DatabaseConnection.java
@@ -1,5 +1,38 @@
 package com.cusat.hackathon.dbconn;
 
-public class DatabaseConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DatabaseConnection {
+	
+	private static Connection con = null;
+	
+	static {
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver loaded");
+		} catch (ClassNotFoundException e){
+			System.out.println("Class not loaded "+e);
+		}
+		
+	}
+	public static Connection getConnectivity(){
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "admin", "system");
+			System.out.println("connection successful"); 
+		} catch (SQLException e) {
+			System.out.println("ERROR: Unable to Connect to Database.");
+		}
+		
+		return con;
+	}
+	public static void closeConnectivity() {
+		try{
+			con.close();
+		}catch(SQLException e){
+			System.out.println("Connection not closed "+ e);
+		}
+		
+	}
 }
