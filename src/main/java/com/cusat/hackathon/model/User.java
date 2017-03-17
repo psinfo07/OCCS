a6de7df14951279eb50f3d3b2d886d9400c55962
BHASKAR <BHASKAR@192.168.0.10>
2017-03-18 01:33:46 +0530

    Correspondence address attr added

--- src/main/java/com/cusat/hackathon/model/User.java
+++ src/main/java/com/cusat/hackathon/model/User.java
@@ -4,6 +4,42 @@ public class User {
 		private PersonalDetail personalDetail;
 		private PermanentAddress permanentAddress;
 		private CorrespondenceAddress correspondenceAddress;
+		/**
+		 * @return the personalDetail
+		 */
+		public PersonalDetail getPersonalDetail() {
+			return personalDetail;
+		}
+		/**
+		 * @param personalDetail the personalDetail to set
+		 */
+		public void setPersonalDetail(PersonalDetail personalDetail) {
+			this.personalDetail = personalDetail;
+		}
+		/**
+		 * @return the permanentAddress
+		 */
+		public PermanentAddress getPermanentAddress() {
+			return permanentAddress;
+		}
+		/**
+		 * @param permanentAddress the permanentAddress to set
+		 */
+		public void setPermanentAddress(PermanentAddress permanentAddress) {
+			this.permanentAddress = permanentAddress;
+		}
+		/**
+		 * @return the correspondenceAddress
+		 */
+		public CorrespondenceAddress getCorrespondenceAddress() {
+			return correspondenceAddress;
+		}
+		/**
+		 * @param correspondenceAddress the correspondenceAddress to set
+		 */
+		public void setCorrespondenceAddress(CorrespondenceAddress correspondenceAddress) {
+			this.correspondenceAddress = correspondenceAddress;
+		}
 		
 		
 }
