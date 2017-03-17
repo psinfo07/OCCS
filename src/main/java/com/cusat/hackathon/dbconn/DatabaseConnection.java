6398980036d71a1ef7b7df0328ba360f12427572
BHASKAR <BHASKAR@192.168.0.10>
2017-03-18 04:12:31 +0530

    latest

--- src/main/java/com/cusat/hackathon/dbconn/DatabaseConnection.java
+++ src/main/java/com/cusat/hackathon/dbconn/DatabaseConnection.java
@@ -1,25 +1,29 @@
 package com.cusat.hackathon.dbconn;
 
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseConnection {
+	static Properties properties = new Properties();
 	
 	private static Connection con = null;
 	
 	static {
 		try{
-			Class.forName("com.mysql.jdbc.Driver");
+			properties.load(new FileInputStream("db.properties"));
+			Class.forName(properties.getProperty("DB_DRIVER_CLASS"));
 			System.out.println("Driver loaded");
-		} catch (ClassNotFoundException e){
+		} catch (Exception e){
+			
 			System.out.println("Class not loaded "+e);
-		}
-		
+		}	
 	}
 	public static Connection getConnectivity(){
 		try {
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ocsproject", "", "");
+			con = DriverManager.getConnection(properties.getProperty("DB_URL"), properties.getProperty("DB_USERNAME"), properties.getProperty("DB_PASSWORD"));
 			System.out.println("connection successful"); 
 		} catch (SQLException e) {
 			System.out.println("ERROR: Unable to Connect to Database.");
@@ -35,7 +39,7 @@ public class DatabaseConnection {
 		}
 		
 	}
-	/**public static void main(String args[]){
+	/*public static void main(String args[]){
 		getConnectivity();
 	}*/
 }
