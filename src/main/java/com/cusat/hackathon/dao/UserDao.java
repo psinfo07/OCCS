0c233e3084cf3802446f37a0f9b41fd96382abc6
PRAKASH <PRAKASH@192.168.0.9>
2017-03-18 21:09:07 +0530

    Updated

--- src/main/java/com/cusat/hackathon/dao/UserDao.java
+++ src/main/java/com/cusat/hackathon/dao/UserDao.java
@@ -4,9 +4,11 @@ import com.cusat.hackathon.model.User;
 
 public class UserDao {
 	public boolean createUser(User user){
-		
-		
 		return true;
-		
+	}
+	
+	public User userProfile(User user){
+		String userId=user.getPersonalDetail().getEmailId();
+		return user;
 	}
 }
