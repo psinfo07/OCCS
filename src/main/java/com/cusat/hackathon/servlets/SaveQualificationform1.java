865bfa990194a378b368fdcde6195ca274d98db7
BHASKAR <BHASKAR@192.168.0.12>
2017-03-26 02:48:06 +0530

    DFDFG

--- src/main/java/com/cusat/hackathon/servlets/SaveQualificationform1.java
+++ src/main/java/com/cusat/hackathon/servlets/SaveQualificationform1.java
@@ -15,7 +15,7 @@ public class SaveQualificationform1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request,HELLO HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter pw= response.getWriter();
