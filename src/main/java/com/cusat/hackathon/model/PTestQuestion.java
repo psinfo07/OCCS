6872a886205910dc10968963d0b0981b5593d44e
BHASKAR <BHASKAR@DESKTOP-42D682A>
2017-04-01 15:23:53 +0530

    Ptest added

---
+++ src/main/java/com/cusat/hackathon/model/PTestQuestion.java
@@ -0,0 +1,57 @@
+package com.cusat.hackathon.model;
+
+public class PTestQuestion {
+	private String sectorCode;
+	private String question;
+	
+	private String option1;
+	private String option2;
+	/**
+	 * @return the sectorCode
+	 */
+	public String getSectorCode() {
+		return sectorCode;
+	}
+	/**
+	 * @param sectorCode the sectorCode to set
+	 */
+	public void setSectorCode(String sectorCode) {
+		this.sectorCode = sectorCode;
+	}
+	/**
+	 * @return the question
+	 */
+	public String getQuestion() {
+		return question;
+	}
+	/**
+	 * @param question the question to set
+	 */
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	/**
+	 * @return the option1
+	 */
+	public String getOption1() {
+		return option1;
+	}
+	/**
+	 * @param option1 the option1 to set
+	 */
+	public void setOption1(String option1) {
+		this.option1 = option1;
+	}
+	/**
+	 * @return the option2
+	 */
+	public String getOption2() {
+		return option2;
+	}
+	/**
+	 * @param option2 the option2 to set
+	 */
+	public void setOption2(String option2) {
+		this.option2 = option2;
+	}
+}
